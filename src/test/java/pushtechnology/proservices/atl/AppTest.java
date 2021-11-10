5b150d80de4961c3e75fd2476e53f83f4e5d9c52
Dave Carson <dcarson@pushtechnology.com>
2021-11-10 16:01:05 +0000

    FB27826 mvn shell

---
+++ src/test/java/pushtechnology/proservices/atl/AppTest.java
@@ -0,0 +1,20 @@
+package pushtechnology.proservices.atl;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
